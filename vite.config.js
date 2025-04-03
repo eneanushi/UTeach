a09d46d51f27412ea5f544b950cad0a8cd9e3776
eneanushi <eneanushi12334@gmail.com>
2025-04-03 02:52:24 -0400

    Update build configuration and fix logo path

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/UTeach/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true,
+  }
 })
