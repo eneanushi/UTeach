5ecf92cc7ad34e54f46e61508113d417965eaf77
eneanushi <eneanushi12334@gmail.com>
2025-04-03 01:58:11 -0400

    Fix: Simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/UTeach/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: true,
-  },
+  base: './'
 })
